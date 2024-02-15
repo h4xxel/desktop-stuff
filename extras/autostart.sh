8ccf45acbaa03323019b71ad2efb996dd19305d0
Axel Isaksson <isaksson.axel@telia.com>
2024-02-15 23:17:25 +0100

    config file updates

--- extras/autostart.sh
+++ extras/autostart.sh
@@ -2,11 +2,14 @@
 nitrogen --restore
 
 #/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+xsettingsd &
+dunst &
 /usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
 nm-applet &
 blueman-applet &
 firewall-applet &
 fix-touchpad &
+udiskie -Ans -f nemo &
 setxkbmap -option compose:caps
 xmodmap  -e 'keycode 78 = Caps_Lock'
 xmodmap -e "clear mod4"
@@ -14,7 +17,15 @@ xmodmap -e "clear mod1"
 xmodmap -e "add mod4 = Alt_L"
 xmodmap -e "add mod1 = Super_L"
 
+
 #syndaemon -i 0.25 -t -d
+#
+autorandr -c
 
 xss-lock -- custom-lock &
-nextcloud &
+nextcloud --background &
+keepassxc &
+#pasystray &
+
+xset s off
+xset -dpms
